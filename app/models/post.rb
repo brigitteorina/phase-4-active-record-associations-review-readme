9728659d076a85b20ce54a694bf864ebd66431c6
Brigitte <brigitteorina@gmail.com>
2023-07-04 11:51:08 +0300

    complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+    belongs_to :author
+    has_many :post_tags
+    has_many :tags, through: :post_tags
 end
+
+post = Post.new(title: "Web Development for Dogs")
+new_author = post.create_author(name: "Lasandra Gulgowski")
+post.save
